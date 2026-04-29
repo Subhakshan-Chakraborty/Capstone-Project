7fc06317519effd6dff5ffb2ed9bfc9c855c29b4
subhakshanchakraborty <subhakshan.chakraborty@tudip.com>
2026-04-29 14:30:35 +0530

    changes

--- backend/django/config/settings.py
+++ backend/django/config/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
  
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
